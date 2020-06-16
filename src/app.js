3a23ecada089ea6d2fac829b74a5d120baf7c357
travis skyles <travis.skyles@gmail.com>
2020-06-15 18:15:43 -0700

    created user model

--- src/app.js
+++ src/app.js
@@ -1,21 +1,31 @@
 'use strict';
 
+const errorHandlers = require('./middleware/errors');
 
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
+// const router = express.Router();
 
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.json);
+// app.use(express.json);
 app.use(express.urlencoded({extended: true}));
 
 
 
 
-app.
+app.get('/', (req, res) => {
+  res.send('hello World')
+})
+// router.post('/users', (req, res, next) => {
+
+// })
+
+app.get('*', errorHandlers[404]);
+app.use(errorHandlers[500]);
 
 module.exports = {
   server: app,
---
+++ src/models/user-model.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+
+const User = new mongoose.Schema({
+  first_name: {type: String, required: true},
+  last_name: {type: String, required: true },
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  role: {type: String, default: 'user', enum: ['admin', 'editor', 'user']},
+})
+
+module.export = mongoose.model('User', User);
