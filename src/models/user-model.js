c356703e978d550a826d40cfc6e8a8330604d979
travis skyles <travis.skyles@gmail.com>
2020-06-15 18:20:09 -0700

    added _id to User model

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 
 const User = new mongoose.Schema({
+  _id: {type: mongoose.ObjectId},
   first_name: {type: String, required: true},
   last_name: {type: String, required: true },
   email: {type: String, required: true, unique: true},
