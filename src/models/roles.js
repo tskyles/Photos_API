cb8b2a74bc6e1646a3bcf271e8994ae2710ee5c6
travis skyles <travis.skyles@gmail.com>
2020-06-16 16:53:44 -0700

    created roles model

---
+++ src/models/roles.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const Roles = new mongoose.Schema({
+  type: { type: String, required: true, enum: ['admin', 'editor', 'user'] },
+  capabilities: { type: Array, required: true },
+});
+
+module.exports = mongoose.model('Roles', Roles);
