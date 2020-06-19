e549babed5e8723e40efeaffde062adb6d3d6b9b
travis skyles <travis.skyles@gmail.com>
2020-06-18 23:05:53 -0700

    modified and cleaned up user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,15 +4,12 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user');
-const helpers = require('../helpers/helpers');
 const basicAuth = require('../middleware/auth/basic');
 const bearerAuth = require('../middleware/auth/bearer');
 
 
 const createUser = async (req, res, next) => {
-  console.log(req.body);
   let user = new User(req.body);
-
   let savedUser = await user.save();
 
   res.status(200).send(savedUser);
