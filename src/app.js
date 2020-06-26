57725d4522bba4ce4517b841926d6aeeacefd13f
travis skyles <travis.skyles@gmail.com>
2020-06-25 19:05:28 -0700

    modifed cors origin

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 // const router = express.Router();
-// app.use(cors({ credentials: true, origin: 'http://localhost:3001' }));
-app.use(cors());
+app.use(cors({ credentials: true, origin: 'http://localhost:3001' }));
+// app.use(cors());
 app.use(cookieParser())
 app.use(morgan('dev'));
 app.use(express.json());
